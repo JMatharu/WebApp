94c7d2be9696c40c0ea6ee9676b234c48877dc42
Jagdeep Matharu <Jagdeepmatharu88@gmail.com>
2015-05-20 16:43:37 -0400

    Changing Source and target for compiling

---
+++ src/main/java/com/jmatharu/mavenwebapp/MyClass.java
@@ -0,0 +1,11 @@
+package com.jmatharu.mavenwebapp;
+
+import java.util.*;
+
+public class MyClass
+{
+    public static void main(String[] args)
+    {
+        List<String> stringList = new ArrayList<String>();
+    }
+}
